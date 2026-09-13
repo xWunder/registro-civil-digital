08d0fd6cb4edb17664021d35203ccf55da837552
Mijael <emmarquinar@unitru.edu.pe>
2026-09-12 23:56:18 -0500

    Define formato de registros binarios de longitud fija

---
+++ RegistroCivil.Web/FileStorage/FormatoArchivoActas.cs
@@ -0,0 +1,25 @@
+namespace RegistroCivil.Web.FileStorage;
+
+public static class FormatoArchivoActas
+{
+    public const string Firma = "RCDIG001";
+    public const int Version = 1;
+
+    public const int TamanoCabecera = 64;
+    public const int TamanoRegistro = 512;
+
+    public const int CaracteresNumeroActa = 20;
+    public const int CaracteresDni = 8;
+    public const int CaracteresApellido = 40;
+    public const int CaracteresNombres = 60;
+    public const int CaracteresUbigeo = 6;
+    public const int CaracteresLugar = 60;
+
+    public const int TamanoEntradaIndiceNumero = 48;
+    public const int TamanoEntradaIndiceDni = 24;
+
+    public static long CalcularOffset(long posicion)
+    {
+        return TamanoCabecera + posicion * TamanoRegistro;
+    }
+}
